d334f1dac46ad45a66f5d37349ba97f71a412c2a
Harshith Kumar K <harshithkumarkumar30@gmail.com>
2026-03-15 23:24:59 +0530

    done with my first api in fastapi

--- database.py
+++ database.py
@@ -3,12 +3,13 @@ from dotenv import load_dotenv;
 import os;
 from sqlalchemy.orm import sessionmaker;
 from sqlalchemy.ext.declarative import declarative_base
+from urllib.parse import quote_plus
 
 
 load_dotenv()
 
 MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_PASSWORD = quote_plus(os.getenv("MYSQL_PASSWORD"))
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_PORT = os.getenv("MYSQL_PORT")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
